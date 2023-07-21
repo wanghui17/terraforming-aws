6f4888ca8b3d171db122e9f881b6a36fe70307b4
huiwang <huiwa@vmware.com>
2023-07-21 22:54:46 +0800

    fix error

--- modules/infra/dns.tf
+++ modules/infra/dns.tf
@@ -2,6 +2,7 @@ locals {
   data_zone_id     = "${element(concat(data.aws_route53_zone.pcf_zone.*.zone_id, [""]), 0)}"
   resource_zone_id = "${element(concat(aws_route53_zone.pcf_zone.*.zone_id, [""]), 0)}"
   zone_id          = "${var.hosted_zone == "" ? local.resource_zone_id : local.data_zone_id}"
+  _count = "${var.hosted_zone == "" ? 0 : 1}"
 
   hosted_zone_count = "${var.hosted_zone == "" ? 0 : 1}"
 
@@ -16,8 +17,8 @@ data "aws_route53_zone" "pcf_zone" {
 }
 
 resource "aws_route53_zone" "pcf_zone" {
-  # count = "${var.use_route53 ? (1 - local.hosted_zone_count) : 0}"
-  count = "${var.use_route53 ? local.hosted_zone_count : 0}"
+  count = "${var.use_route53 ? (1 - local.hosted_zone_count) : 0}"
+  # count = "${var.use_route53 ? local.hosted_zone_count : 0}"
   name = "${var.env_name}.${var.dns_suffix}"
   tags = merge(
     var.tags, 
