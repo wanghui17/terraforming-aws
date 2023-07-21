256e800e0e2e32f3636965f9a4805ab48b4bc9e8
huiwang <huiwa@vmware.com>
2023-07-21 23:25:31 +0800

    fix error

--- modules/infra/outputs.tf
+++ modules/infra/outputs.tf
@@ -3,7 +3,7 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = ["${aws_subnet.public_subnets.*.id}"]
+  value = "${aws_subnet.public_subnets.*.id}"
 }
 
 output "deployment_route_table_ids" {
@@ -23,31 +23,31 @@ output "vms_security_group_id" {
 }
 
 output "public_subnet_availability_zones" {
-  value = ["${aws_subnet.public_subnets.*.availability_zone}"]
+  value = "${aws_subnet.public_subnets.*.availability_zone}"
 }
 
 output "public_subnet_cidrs" {
-  value = ["${aws_subnet.public_subnets.*.cidr_block}"]
+  value = "${aws_subnet.public_subnets.*.cidr_block}"
 }
 
 output "infrastructure_subnet_ids" {
-  value = ["${aws_subnet.infrastructure_subnets.*.id}"]
+  value = "${aws_subnet.infrastructure_subnets.*.id}"
 }
 
 output "infrastructure_subnets" {
-  value = ["${aws_subnet.infrastructure_subnets.*.id}"]
+  value = "${aws_subnet.infrastructure_subnets.*.id}"
 }
 
 output "infrastructure_subnet_availability_zones" {
-  value = ["${aws_subnet.infrastructure_subnets.*.availability_zone}"]
+  value = "${aws_subnet.infrastructure_subnets.*.availability_zone}"
 }
 
 output "infrastructure_subnet_cidrs" {
-  value = ["${aws_subnet.infrastructure_subnets.*.cidr_block}"]
+  value = "${aws_subnet.infrastructure_subnets.*.cidr_block}"
 }
 
 output "infrastructure_subnet_gateways" {
-  value = ["${data.template_file.infrastructure_subnet_gateways.*.rendered}"]
+  value = "${data.template_file.infrastructure_subnet_gateways.*.rendered}"
 }
 
 output "aws_lb_interface_endpoint_ips" {
