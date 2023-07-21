572cf4941e3dd44638b66c227e92f8c0d18ac8bd
huiwang <huiwa@vmware.com>
2023-07-22 00:24:05 +0800

    fix error

--- modules/pks/lb.tf
+++ modules/pks/lb.tf
@@ -36,7 +36,7 @@ resource "aws_lb" "pks_api" {
   load_balancer_type               = "network"
   enable_cross_zone_load_balancing = true
   internal                         = false
-  subnets                          = element("${var.public_subnet_ids}",1)
+  subnets                          = ["${var.public_subnet_ids}"]
 }
 
 resource "aws_lb_listener" "pks_api_9021" {
